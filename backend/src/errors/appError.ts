b80f01c0ee0aa7a9bb9fb2790736508d08e19861
Gabriel Ferreira Machado <53434033+1GabrielMachado1@users.noreply.github.com>
2022-04-25 22:14:13 -0300

    Create README.md

--- backend/src/errors/appError.ts
+++ backend/src/errors/appError.ts
@@ -2,7 +2,7 @@ export default class AppError {
   readonly message: string
   readonly statusCode: number
 
-  constructor(msg: string = "Ops! Erro inesperado.", code: number = 400) {
+  constructor(msg: string = "Unexpected error", code: number = 400) {
     this.message = msg
     this.statusCode = code
   }
